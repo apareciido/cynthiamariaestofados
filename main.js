73fba1ac2c6be8dfc94a863562b10e3b3eb09d65
apareciido <oaparecido363@gmail.com>
2024-10-31 16:10:54 -0300

    navbar funcionando

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-const toggleBtn = document.querySelector('.toggle_btn')
-const toggleBtnIcon = document.querySelector('.toggle_btn')
-const dropDownMenu = document.querySelector('.dropdown_menu')
 
-toggleBtn.onclick = function () {
-    dropDownMenu.classList.toggle('open')
-    const isOpen = dropDownMenu.classList.contains('open')
-
-    toggleBtnIcon.classList = isOpen
-        ? 'fa-solid fa-xmark'
-        : 'fa-solid fa-bars'
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
 }
